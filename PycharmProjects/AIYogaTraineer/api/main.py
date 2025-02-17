21dc9ba86dfa7794893703d7a5abb011302b719b
Neha Garg <nehagarg@Nehas-MacBook-Air.local>
2025-02-17 16:11:41 +0530

    changes

--- PycharmProjects/AIYogaTraineer/api/main.py
+++ PycharmProjects/AIYogaTraineer/api/main.py
@@ -13,5 +13,5 @@ app.include_router(process_image.router)
 def home():
     return {"message": "Hello, FastAPI!"}
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8001))  # Default to 8000 if PORT is not set
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
