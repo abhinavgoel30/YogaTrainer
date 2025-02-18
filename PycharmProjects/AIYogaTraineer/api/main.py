cb0b1cde6030e15fe8cc3fe09ed251b5912a1aaf
Neha Garg <nehagarg@Nehas-MacBook-Air.local>
2025-02-18 12:27:43 +0530

    fchanges

--- PycharmProjects/AIYogaTraineer/api/main.py
+++ PycharmProjects/AIYogaTraineer/api/main.py
@@ -1,6 +1,3 @@
-import os
-
-import uvicorn
 from fastapi import FastAPI
 import process_image  # Import the endpoints module
 
@@ -12,6 +9,3 @@ app.include_router(process_image.router)
 @app.get("/")
 def home():
     return {"message": "Hello, FastAPI!"}
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
